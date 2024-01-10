51053c1c42da7e87418562b27711d5aba07c88fd
Matthew Mols <matt@pgedge.com>
2024-01-09 22:19:04 -0600

    update route, redeploy

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -6,7 +6,7 @@ export async function GET(request) {
 	const cookieStore = cookies();
 	const token = cookieStore.get('token');
 
-	return new Response('Hello, Next.js!', {
+	return new Response('Hello, World!', {
 		status: 200,
 		headers: { 'Set-Cookie': `token=${token}` },
 	});
