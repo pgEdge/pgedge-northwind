eb356c8ca0db9edd8d46914f14aba916993f7152
Matthew Mols <matt@pgedge.com>
2024-01-09 22:02:45 -0600

    initial commit

---
+++ src/app/api/hello/route.js
@@ -0,0 +1,13 @@
+import { cookies } from 'next/headers';
+
+export const runtime = 'edge';
+
+export async function GET(request) {
+	const cookieStore = cookies();
+	const token = cookieStore.get('token');
+
+	return new Response('Hello, Next.js!', {
+		status: 200,
+		headers: { 'Set-Cookie': `token=${token}` },
+	});
+}
