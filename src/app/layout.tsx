6c52d8e913722f437d8ca79ebd177108de42e7d6
Matthew Mols <matt@pgedge.com>
2024-01-11 16:06:47 -0600

    improve header styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,57 +1,61 @@
-'use client'
+"use client";
 
-import '@mantine/core/styles.css';
-import 'mantine-datatable/styles.layer.css';
+import "@mantine/core/styles.css";
+import "mantine-datatable/styles.layer.css";
 
-import { ColorSchemeScript, MantineProvider, Burger, AppShell, Text} from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
-import { NavbarSimple } from './components/NavbarSimple/NavbarSimple';
+import {
+  Flex,
+  ColorSchemeScript,
+  MantineProvider,
+  Title,
+  Burger,
+  AppShell,
+  Text,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { NavbarSimple } from "./components/NavbarSimple/NavbarSimple";
 
-
-export default function Document({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-
+export default function Document({ children }: { children: React.ReactNode }) {
   const [opened, { toggle }] = useDisclosure();
 
   return (
     <>
-    <html lang="en">
-      <head>
-        <ColorSchemeScript />
-      </head>
-      <body>
-        <MantineProvider>
-          <AppShell
-            header={{ height: 60 }}
-            navbar={{
-              width: 300,
-              breakpoint: 'sm',
-              collapsed: { mobile: !opened },
-            }}
-            padding="md"
-          >
-            <AppShell.Header>
-              <Burger
-                opened={opened}
-                onClick={toggle}
-                hiddenFrom="sm"
-                size="sm"
-              />
-              <Text size="lg" fw={700}>
-                Northwind Traders
-              </Text>
-            </AppShell.Header>
+      <html lang="en">
+        <head>
+          <ColorSchemeScript />
+        </head>
+        <body>
+          <MantineProvider>
+            <AppShell
+              header={{ height: 40 }}
+              navbar={{
+                width: 300,
+                breakpoint: "sm",
+                collapsed: { mobile: !opened },
+              }}
+              padding="md"
+            >
+              <AppShell.Header>
+                <Flex>
+                  <Burger
+                    opened={opened}
+                    onClick={toggle}
+                    hiddenFrom="sm"
+                    size="md"
+                  />
+                  <Title pt={2} pl={15} order={3}><strong>Northwind</strong> Traders</Title>
+                </Flex>
+              </AppShell.Header>
 
-            <AppShell.Navbar p="md"><NavbarSimple></NavbarSimple></AppShell.Navbar>
+              <AppShell.Navbar p="md">
+                <NavbarSimple></NavbarSimple>
+              </AppShell.Navbar>
 
-            <AppShell.Main>{children}</AppShell.Main>
+              <AppShell.Main>{children}</AppShell.Main>
             </AppShell>
-        </MantineProvider>
-      </body>
-    </html>
+          </MantineProvider>
+        </body>
+      </html>
     </>
-  )
+  );
 }
