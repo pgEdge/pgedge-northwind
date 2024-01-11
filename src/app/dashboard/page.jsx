e1b50442efca5c6f9bf82ea83206986fac93a189
Matthew Mols <matt@pgedge.com>
2024-01-10 23:52:50 -0600

    debug

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -5,15 +5,13 @@ import { getUser, getDbInfo } from '../data/api';
 
 export default async function Dashboard() {
 
-  const userInfo = JSON.stringify(await getUser())
-  const dbInfo = JSON.stringify(await getDbInfo())
+  // const userInfo = JSON.stringify(await getUser())
+  // const dbInfo = JSON.stringify(await getDbInfo())
   return (
       <>
         <Title order={3}>Dashboard</Title>
-        <Title order={4}>User Info</Title>
-        <Text>{userInfo}</Text>
-        <Title order={4}>DB Info</Title>
-        <Text>{dbInfo}</Text>
+        <Title order={4}>Env</Title>
+        <Text>{process.env}</Text>
       </>
   )
 }
