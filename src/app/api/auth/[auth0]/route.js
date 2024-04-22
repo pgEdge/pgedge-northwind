95e38c2e32942cd7669e9352c7202e56e9b578b6
KnockOutEZ <ktowhid20@gmail.com>
2024-04-22 19:13:35 +0600

    fixed logout issue

--- src/app/api/auth/[auth0]/route.js
+++ src/app/api/auth/[auth0]/route.js
@@ -1,9 +1,16 @@
-import { handleAuth } from "@auth0/nextjs-auth0/edge";
+import { handleAuth,handleLogout } from "@auth0/nextjs-auth0/edge";
 import util from "node:util";
 
 export const runtime = "edge";
 
+const logoutUrl = [
+  `${process.env.AUTH0_ISSUER_BASE_URL}/v2/logout?`,
+  `client_id=${process.env.AUTH0_CLIENT_ID}`,
+  `&returnTo=${process.env.AUTH0_BASE_URL}`,
+];
+
 export const GET = handleAuth({
+  logout: handleLogout({ returnTo: logoutUrl.join('') }),
   onError: (req, error) => {
     console.error(error);
     console.log("error request", req.nextUrl);
